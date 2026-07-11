e0c7cb96c1f37df6fec23794979c941a9e3b8c8a
mooleshacat <mooleshacat@catspeed.cc>
2026-07-11 00:14:27 -0400

    fix: license change from GPLv3 -> GPLv2 (#3)

--- etc/config.sh
+++ etc/config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2026 mooleshacat <mooleshacat@catspeed.cc>
 
 # ==============================================================================
