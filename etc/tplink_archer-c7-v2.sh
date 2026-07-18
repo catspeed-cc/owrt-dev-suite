0c126740a7e2636f6398dd3160c49803a562b2d1
mooleshacat <mooleshacat@catspeed.cc>
2026-07-17 20:19:55 -0400

    fix: add mainline supported TP-Link Archer C7 v2 configuration example file

---
+++ etc/tplink_archer-c7-v2.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2026 mooleshacat <mooleshacat@catspeed.cc>
+
+
+# ==================================================================
+# User Configuration Variables                       (INSTRUCTIONS)
+# ==================================================================
+#
+# Define your configuration in this file. Ensure the required
+# sections are filled out correctly:
+#
+#  - OPWNWRT SOURCE & REPO
+#  - DEVICE IDENTIFICATION
+#  - BASE DIRECTORIES
+#
+# Variables not required will be commented
+# You may uncomment to override the auto-derived defaults
+# It's highly recommended to use the symlinks & auto-derived paths
+#
+# owrt-dev-suite keeps your work and projects directories organized
+#
+
+
+# ==================================================================
+# OPENWRT SOURCE & REPO                                  (REQUIRED)
+# ==================================================================
+
+# Remote Alias
+# Use a unique remote ailias for each remote to keep them separate
+# Non-functional, but set anyways for when multi-port arrives
+OWRT_REMOTE_ALIAS="catspeed-cc"
+
+# YOUR OPENWRT FORK REPOSITORY INFO
+# Your fork of the openwrt repository where your ports will live
+OWRT_FORK_REPO="https://github.com/catspeed-cc/openwrt.git"
+
+# OpenWRT version to base the port branch off of
+OWRT_VERSION="25.12"
+
+# Base openwrt branch (which openwrt version?)
+# Base branch to auto create custom port/model branches from
+# If you want to target earlier openwrt versions, change this
+OWRT_BASE_BRANCH="openwrt-$OWRT_VERSION"
+
+# Target custom port/model branch (your port branch)
+# We added the BASE_BRANCH so we can build multiple openwrt versions
+# provided we add a new branch, backport it, and create configs
+OWRT_TARGET_BRANCH="openwrt-$OWRT_VERSION"
+
+# DEVICE_SUPPORTED
+# Mark as supported under two conditions, either:
+#  - device is already supported in openwrt mainline
+#  - your port is complete (caldata from art, driver patches complete)
+DEVICE_SUPPORTED="true"
+
+# ENABLE_SYMLINK_SHORTCUTS
+# Each model will have a symlink in root to your port model's directory
+# containing the DTS, image-out, rawmod, patchmod, caldata, directories
+ENABLE_SYMLINK_SHORTCUTS="true"
+
+
+# ==================================================================
+# DEVICE IDENTIFICATION                                  (REQUIRED)
+#
+# Device information is used for display and path derivation
+# These are required for operation. Please configure sane values
+# ==================================================================
+
+# CURRENT PORT INFORMATION (for display only)
+# TODO: multi-port and git support (using infinite string)
+OWRT_MFR="TP-Link"
+OWRT_MODEL="Archer-C7-v2"
+OWRT_SOC="ath79"
+OWRT_SOC_CLASS="ath79"
+
+
+# ==================================================================
+# BASE DIRECTORIES                                       (REQUIRED)
+#
+# These base paths are used below to derive other paths.
+# These are required for operation. Please configure sane values
+# ==================================================================
+
+WORK_DIR="$HOME/work"
+PROJECT_DIR="$HOME/projects"
+
+# Example override: $PROJECT_DIR/openwrt-dev
+OWRT_DEV_DIR="$PROJECT_DIR/openwrt-dev"
+
+# Needed for both IMGDIR_CPY and WEBSERVER_CPY features
+IMGDIR_SRC="$OWRT_DEV_DIR/bin/targets/ath79/generic"
+
+# ==================================================================
+# SHORTCUT SYMLINKS                                      (OPTIONAL)
+#
+# Enable sudo commands (will ask for password). You can disable this
+# if you use root account, but it is not reccommended.
+#
+# It is *HIGHLY* reccommended to use a regular user account that has
+# access to sudo with password (encouraged)
+#
+# Default: false
+# Required for auto-dependency install and auto webserver_cpy setup
+# ==================================================================
+
+SUDO_ENABLE=true
+
+
+# =================================================================
+# COPY IMAGES TO WORK_IMAGEOUT_DIR                      (OPTIONAL)
+#
+# These paths are used to copy build images into WORK_IMAGEOUT_DIR
+# Users can override any of these by uncommenting and changing them
+#
+# Default: true
+# ==================================================================
+
+# DO_IMGDIR_CPY: Enable image copy to work dir on build
+DO_IMGDIR_CPY=true
+
+
+# =================================================================
+# COPY IMAGES TO WEBSERVER DIR                          (OPTIONAL)
+#
+# This feature will copy the images to your webserver shared
+# directory. It will organize in similar manner to your WORK_DIR.
+# A link will be made from the shared directory to your webserver
+# downloads/ directory.
+#
+# Users can override any of these by uncommenting and changing them
+# ==================================================================
+#
+# REQUIRED:
+#  - fully configured webserver (NGINX, Lighttpd, Apache, etc.)
+#  - configured downloads/ directory with file listing enabled
+#
+
+# DO_IMGDIR_CPY: Enable image copy to webserver shared dir on build
+DO_WEBSERVER_CPY=true
+
+# Webserver configuration
+WEBSERVER_USER="www-data"
+WEBSERVER_SHARED_GROUP="openwrt-build"
+WEBSERVER_SHARED_DIR="/srv/openwrt-builds"
+WEBSERVER_ROOT="/var/www/catspeed.cc/downloads"
+
+
+# Commands to restart your webserver (choose one)
+#
+# `sudo` should be required, because you use a user account right?
+# you certainly wouldn't run as root like a neanderthal, not you.
+
+# Nginx
+WEBSERVER_RESTART_CMD="sudo systemctl restart nginx"
+
+# Lighttpd
+# WEBSERVER_RESTART_CMD="sudo systemctl restart lighttpd"
+
+# Apache
+# WEBSERVER_RESTART_CMD="sudo systemctl restart apache2"
+
+# Direct init script
+# WEBSERVER_RESTART_CMD="sudo /etc/init.d/webserver reload"
+
+
+
+
+# DO NOT ENABLE FOR SUPPORTED DEVICES!
+DO_DTS_CPY=false
+DO_DRIVERMOD_CPY=false
+DO_CALDATA_CPY=false
