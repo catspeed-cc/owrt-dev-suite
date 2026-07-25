b250e11b1e8ff468c81fab1efa224635d1645dba
mooleshacat <mooleshacat@catspeed.cc>
2026-07-25 00:50:10 -0400

    fix: update code to support new '.config' file location 'etc/*/owrt/*'

--- lib/file-io.functions.sh
+++ lib/file-io.functions.sh
@@ -290,7 +290,7 @@ copy_to_webserver() {
 #   sync_config_to_etc_dir
 # =============================================================================
 function sync_config_to_dev_dir() {
-    local owrt_config_src="${SCRIPT_DIR}/etc/${OWRT_VERSION}/${OWRT_MFR_LOWER}_${OWRT_MODEL_LOWER}.config"
+    local owrt_config_src="${SCRIPT_DIR}/etc/${OWRT_VERSION}/owrt/${OWRT_MFR_LOWER}_${OWRT_MODEL_LOWER}.config"
 
     if [[ -f "$owrt_config_src" ]]; then
         if cp "$owrt_config_src" "$OWRT_DEV_DIR/.config"; then
@@ -333,7 +333,7 @@ function sync_config_from_dev_dir() {
         if [[ -f "$OWRT_DEV_DIR/.config" ]]; then
             # We found a .config to copy. Now where to copy it?
             # Replace ".build" in CONFIG_FILE with ".config"
-            local config_dest="${SCRIPT_DIR}/etc/${OWRT_VERSION}/${CONFIG_FILE%.build}.config"
+            local config_dest="${SCRIPT_DIR}/etc/${OWRT_VERSION}/owrt/${CONFIG_FILE%.build}.config"
 
             # Ensure the target directory exists (in case it's the first run)
             mkdir -p "$(dirname "$config_dest")"
@@ -374,7 +374,7 @@ function sync_config_from_dev_dir() {
         return 0
     fi
 
-    # Copy the config back to WORK_DIR
+    # Copy the config back to owrt-dev-suite repo dir (or wherever it came from, as logged in the file)
     if [[ "$OWRTDS_INTERACTIVE" == "false" ]]; then
         echo " >>> Synchronizing .config back to work directory..."
     fi
